88336c1eef3736a920fbb63ee993434b31531c75
Camilo Acuña <camilo.aku@gmail.com>
2022-09-28 02:49:22 +0800

    feature(event): add event package with event structs definitions

---
+++ internal/event/event.go
@@ -0,0 +1,39 @@
+package event
+
+import (
+	"github.com/darchlabs/synchronizer-v2/internal/blockchain"
+)
+
+type EventStorage interface {
+	InsertEventData(e *Event, data []blockchain.LogData) (int64, error)
+	UpdateEvent(e *Event) error
+}
+
+type Event struct {
+	Address string `json:"address"`
+	LatestBlockNumber int64 `json:"LatestBlockNumber"`
+	Abi *Abi `json:"abi"`
+}
+
+func (e *Event) UpdateLatestBlock(lbn int64, storage EventStorage) error {
+	// change latest block number value
+	e.LatestBlockNumber = lbn;
+
+	// update event in database
+	err := storage.UpdateEvent(e)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *Event) InsertData(data []blockchain.LogData, storage EventStorage) (int64, error) {
+	// insert event data to event
+	count, err := storage.InsertEventData(e, data)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
