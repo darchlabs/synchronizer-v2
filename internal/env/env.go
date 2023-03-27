fa2a961a48574e2e683b4fbdf99422cdfdb1b70b
Camilo Acuña <camilo.aku@gmail.com>
2023-03-27 19:59:33 +0700

    reafactor(env): add support to databse_dsn env value

--- internal/env/env.go
+++ internal/env/env.go
@@ -1,8 +1,8 @@
 package env
 
 type Env struct {
-	IntervalSeconds  string `envconfig:"interval_seconds" required:"true"`
-	DatabaseFilepath string `envconfig:"database_filepath" required:"true"`
-	Port             string `envconfig:"port" required:"true"`
-	Debug            bool   `envconfig:"debug" required:"true"`
+	IntervalSeconds string `envconfig:"interval_seconds" required:"true"`
+	DatabaseDSN     string `envconfig:"database_dsn" required:"true"`
+	Port            string `envconfig:"port" required:"true"`
+	Debug           bool   `envconfig:"debug" default:"false"`
 }
