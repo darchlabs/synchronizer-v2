cb2022435bab1c3a4ee0d976213449500c68fc28
Camilo Acuña <camilo.aku@gmail.com>
2023-03-29 05:41:11 +0700

    refactor(synchronizer): sinchronizers interface was changed

--- synchronizer.go
+++ synchronizer.go
@@ -6,15 +6,19 @@ import (
 )
 
 type EventStorage interface {
-	ListEventsByAddress(address string) ([]*event.Event, error)
-	ListEvents() ([]*event.Event, error)
+	ListAllEvents() ([]*event.Event, error)
+	ListEvents(limit int64, offset int64) ([]*event.Event, error)
+	ListEventsByAddress(address string, limit int64, offset int64) ([]*event.Event, error)
 	GetEvent(address string, eventName string) (*event.Event, error)
-	GetEventByID(id int64) (*event.Event, error)
+	GetEventByID(id string) (*event.Event, error)
 	InsertEvent(e *event.Event) (*event.Event, error)
 	UpdateEvent(e *event.Event) error
 	DeleteEvent(address string, eventName string) error
-	ListEventData(address string, eventName string) ([]*event.EventData, error)
+	ListEventData(address string, eventName string, limit int64, offset int64) ([]*event.EventData, error)
 	InsertEventData(e *event.Event, data []blockchain.LogData) error
+	GetEventsCount() (int64, error)
+	GetEventCountByAddress(address string) (int64, error)
+	GetEventDataCount(address string, eventName string) (int64, error)
 	Stop() error
 }
 
