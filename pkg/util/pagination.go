38fef5694d5557e39841f8c8432beb2abe10cccf
Camilo Acuña <camilo.aku@gmail.com>
2023-03-29 20:47:26 +0700

    refactor(pagination): add support to sort param

--- pkg/util/pagination.go
+++ pkg/util/pagination.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"math"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,17 +10,23 @@ import (
 const DefaultPage = 1
 const DefaultLimit = 10
 
+const SortAsc = "ASC"
+const SortDesc = "DESC"
+const DefaultSort = "DESC" // asc
+
 type Pagination struct {
-	Page   int64 `json:"page" query:"page"`
-	Limit  int64 `json:"limit" query:"limit"`
-	Offset int64 `json:"offset"`
+	Page   int64  `json:"page" query:"page"`
+	Limit  int64  `json:"limit" query:"limit"`
+	Offset int64  `json:"offset"`
+	Sort   string `json:"sort"`
 }
 
 type PaginationMeta struct {
-	Page          int64 `json:"page"`
-	Limit         int64 `json:"limit"`
-	TotalElements int64 `json:"totalElements"`
-	TotalPages    int64 `json:"totalPages"`
+	Page          int64  `json:"page"`
+	Limit         int64  `json:"limit"`
+	TotalElements int64  `json:"totalElements"`
+	TotalPages    int64  `json:"totalPages"`
+	Sort          string `json:"sort"`
 }
 
 func (p *Pagination) GetPaginationFromFiber(c *fiber.Ctx) error {
@@ -29,13 +36,21 @@ func (p *Pagination) GetPaginationFromFiber(c *fiber.Ctx) error {
 		return err
 	}
 
-	// define default if it's necessary
+	// define default page
 	if p.Page == 0 {
 		p.Page = DefaultPage
 	}
 
+	/// define default limit
 	if p.Limit == 0 {
 		p.Limit = DefaultLimit
+	}
+
+	// define defaul sort
+	if p.Sort == "" || !(strings.ToUpper(p.Sort) == SortAsc || strings.ToUpper(p.Sort) == SortDesc) {
+		p.Sort = strings.ToUpper(SortDesc)
+	} else {
+		p.Sort = strings.ToUpper(p.Sort)
 	}
 
 	// calculate offset
@@ -53,5 +68,6 @@ func (p *Pagination) GetPaginationMeta(totalElements int64) PaginationMeta {
 		Limit:         p.Limit,
 		TotalElements: totalElements,
 		TotalPages:    totalPages,
+		Sort:          p.Sort,
 	}
 }
