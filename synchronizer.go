ce8d59aa36cb784343222fe9baa9a1a1d13c8859
Camilo Acuña <camilo.aku@gmail.com>
2023-03-29 20:47:50 +0700

    refactor(synchronizer): add several changes on base storage interface

--- synchronizer.go
+++ synchronizer.go
@@ -1,21 +1,20 @@
 package synchronizer
 
 import (
-	"github.com/darchlabs/synchronizer-v2/internal/blockchain"
 	"github.com/darchlabs/synchronizer-v2/pkg/event"
 )
 
 type EventStorage interface {
 	ListAllEvents() ([]*event.Event, error)
-	ListEvents(limit int64, offset int64) ([]*event.Event, error)
-	ListEventsByAddress(address string, limit int64, offset int64) ([]*event.Event, error)
+	ListEvents(sort string, limit int64, offset int64) ([]*event.Event, error)
+	ListEventsByAddress(address string, sort string, limit int64, offset int64) ([]*event.Event, error)
 	GetEvent(address string, eventName string) (*event.Event, error)
 	GetEventByID(id string) (*event.Event, error)
 	InsertEvent(e *event.Event) (*event.Event, error)
 	UpdateEvent(e *event.Event) error
 	DeleteEvent(address string, eventName string) error
-	ListEventData(address string, eventName string, limit int64, offset int64) ([]*event.EventData, error)
-	InsertEventData(e *event.Event, data []blockchain.LogData) error
+	ListEventData(address string, eventName string, sort string, limit int64, offset int64) ([]*event.EventData, error)
+	InsertEventData(e *event.Event, data []*event.EventData) error
 	GetEventsCount() (int64, error)
 	GetEventCountByAddress(address string) (int64, error)
 	GetEventDataCount(address string, eventName string) (int64, error)
@@ -26,6 +25,7 @@ type Cronjob interface {
 	Stop() error
 	Restart() error
 	Start() error
+	Halt()
 	GetStatus() string
 	GetSeconds() int64
 	GetError() string
