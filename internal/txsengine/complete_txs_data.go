58b6d04a98a98cc455623b34dee54924f150103d
Camilo Acuña <camilo.aku@gmail.com>
2023-10-12 23:15:03 +0200

    refactor: the request to node was disabled

--- internal/txsengine/complete_txs_data.go
+++ internal/txsengine/complete_txs_data.go
@@ -3,16 +3,13 @@ package txsengine
 import (
 	"context"
 	"fmt"
-	"log"
 	"math/big"
-	"strconv"
 	"time"
 
 	"github.com/darchlabs/synchronizer-v2/pkg/smartcontract"
 	"github.com/darchlabs/synchronizer-v2/pkg/transaction"
 	"github.com/darchlabs/synchronizer-v2/pkg/util"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/params"
 )
 
 type EthClient interface {
@@ -41,38 +38,43 @@ func completeContractTxsData(client EthClient, contract *smartcontract.SmartCont
 				tx.ChainID = fmt.Sprint(util.SupportedNetworks[string(contract.Network)])
 
 				// parse block number
-				blockNum, err := strconv.ParseInt(tx.BlockNumber, 10, 64)
-				if err != nil {
-					log.Printf("WARNING: Failed to parse block number for transaction %s: %v", tx.Hash, err)
-				}
+				// blockNum, err := strconv.ParseInt(tx.BlockNumber, 10, 64)
+				// if err != nil {
+				// 	log.Printf("WARNING: Failed to parse block number for transaction %s: %v", tx.Hash, err)
+				// }
 
-				// get tx fromBalance from node
-				fromBalance, err := client.BalanceAt(context.Background(), common.HexToAddress(tx.From), big.NewInt(blockNum))
-				if err != nil {
-					log.Printf("WARNING: Failed to get fromBalance for transaction %s: %v", tx.Hash, err)
-				}
-				tx.FromBalance = fromBalance.String()
+				// TODO(ca): Temporary disabled because has request to the node
+				// // get tx fromBalance from node
+				// fromBalance, err := client.BalanceAt(context.Background(), common.HexToAddress(tx.From), big.NewInt(blockNum))
+				// if err != nil {
+				// 	log.Printf("WARNING: Failed to get fromBalance for transaction %s: %v", tx.Hash, err)
+				// }
+				// tx.FromBalance = fromBalance.String()
+				tx.FromBalance = "0"
 
-				// get contract balance from node
-				contractBalance, err := client.BalanceAt(context.Background(), common.HexToAddress(contract.Address), big.NewInt(blockNum))
-				if err != nil {
-					log.Printf("WARNING: Failed to get contract balance for transaction %s: %v", tx.Hash, err)
-				} else if contractBalance == nil {
-					log.Printf("WARNING: Failed because contractBalance is nil, hash=%s error=%v", tx.Hash, err)
-					tx.ContractBalance = "0"
-					tx.FromIsWhale = "0"
-				} else {
-					tx.ContractBalance = contractBalance.String()
+				// TODO: Temporary disabled because has request to the node
+				// // get contract balance from node
+				// contractBalance, err := client.BalanceAt(context.Background(), common.HexToAddress(contract.Address), big.NewInt(blockNum))
+				// if err != nil {
+				// 	log.Printf("WARNING: Failed to get contract balance for transaction %s: %v", tx.Hash, err)
+				// } else if contractBalance == nil {
+				// 	log.Printf("WARNING: Failed because contractBalance is nil, hash=%s error=%v", tx.Hash, err)
+				// 	tx.ContractBalance = "0"
+				// 	tx.FromIsWhale = "0"
+				// } else {
+				// 	tx.ContractBalance = contractBalance.String()
 
-					// define if from balance is whale
-					whaleLimit := big.NewFloat(params.Ether * 10000)
-					whaleLimitInt, _ := whaleLimit.Int64()
-					if contractBalance.Int64() > whaleLimitInt {
-						tx.FromIsWhale = "1"
-					} else {
-						tx.FromIsWhale = "0"
-					}
-				}
+				// 	// define if from balance is whale
+				// 	whaleLimit := big.NewFloat(params.Ether * 10000)
+				// 	whaleLimitInt, _ := whaleLimit.Int64()
+				// 	if contractBalance.Int64() > whaleLimitInt {
+				// 		tx.FromIsWhale = "1"
+				// 	} else {
+				// 		tx.FromIsWhale = "0"
+				// 	}
+				// }
+				tx.ContractBalance = "0"
+				tx.FromIsWhale = "0"
 
 				results <- tx
 			}
